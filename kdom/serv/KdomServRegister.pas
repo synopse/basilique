58567769823a581b356a4508459d65494cf5b725
Arnaud Bouchez <fakeaddress@synopse.info>
2019-10-28 09:58:36 +0100

    includes Kindcom Core units skeletons

---
+++ kdom/serv/KdomServRegister.pas
@@ -0,0 +1,78 @@
+/// Kingdom User Registration Core Services
+// - this unit is a part of the freeware Synopse Basilique framework,
+// licensed under a GPL v3 license
+unit KdomObjRegister;
+
+{
+    This file is part of Synopse Basilique framework.
+
+    Synopse Basilique framework. Copyright (C) 2019 Arnaud Bouchez
+      Synopse Informatique - https://synopse.info
+
+  *** BEGIN LICENSE BLOCK *****
+  Version: GPL 3
+
+  This program is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+  The Original Code is Synopse Basilique framework.
+
+  The Initial Developer of the Original Code is Arnaud Bouchez.
+
+  Portions created by the Initial Developer are Copyright (C) 2019
+  the Initial Developer. All Rights Reserved.
+
+  Contributor(s):
+
+  ***** END LICENSE BLOCK *****
+
+
+  Version 0.1
+  - first public release of the Basilique Framework
+
+}
+
+{$I Synopse.inc} // cross-platform and cross-compiler conditional definitions
+
+interface
+
+uses
+  {$ifdef MSWINDOWS}
+  Windows, // for some obscure inlining under Delphi
+  {$endif MSWINDOWS}
+  SysUtils,
+  Classes,
+  Variants,
+  SynCommons,
+  SynTable,
+  SynLog,
+  mORMot,
+  KdomObjUser,
+  KdomObjCommunity;
+
+
+implementation
+
+
+initialization
+  TTextWriter.RegisterCustomJSONSerializerFromTextSimpleType([
+    ]);
+  TTextWriter.RegisterCustomJSONSerializerFromTextBinaryType([
+    ]);
+  TTextWriter.RegisterCustomJSONSerializerFromText([
+    ]);
+  TJSONSerializer.RegisterObjArrayForJSON([
+    ]);
+  TInterfaceFactory.RegisterInterfaces([
+    ]);
+end.
